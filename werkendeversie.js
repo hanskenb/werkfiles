2e81ac24877bac6c3f00b99cb9371752b8156f76
hanskenb <hbontinck@gmaiL.com>
2023-03-05 09:25:18 +0100

    Create werkendeversie.js

---
+++ werkendeversie.js
@@ -0,0 +1,169 @@
+const fs = require('fs');
+const csv = require('csv-parser');
+const https = require('https');
+const axios = require('axios');
+const moment = require('moment-timezone');
+const xml2js = require('xml2js');
+const API_TOKEN = 'your-token';
+
+const data = [];
+
+fs.createReadStream('csvstream.csv')
+  .pipe(csv({
+    separator: ';'
+  }))
+  .on('data', (row) => {
+    const hour = parseInt(row['Van Tijdstip'].split(':')[0]);
+    const energyStr = row['Volume'].replace(',', '.').trim();
+
+    const energy = parseFloat(energyStr);
+
+    if (row['Register'] === 'Afname Nacht' || row['Register'] === 'Afname Dag') {
+      data.push({
+        date: row['\uFEFFVan Datum'],
+        hour,
+        energy
+      });
+    }
+  })
+  .on('end', () => {
+    // Get the oldest and most recent dates in the data array
+    const dates = data.map(row => moment(row.date, 'DD-MM-YYYY'));
+    const oldestDate = moment.min(dates).format('DD-MM-YYYY');
+    const mostRecentDate = moment.max(dates).format('DD-MM-YYYY');
+    const oldestDateFormatted = moment(oldestDate, 'DD-MM-YYYY').format('YYYYMMDD');
+    const mostRecentDateFormatted = moment(mostRecentDate, 'DD-MM-YYYY').format('YYYYMMDD');
+    const formattedStart = oldestDateFormatted + '0000';
+    const formattedEnd = mostRecentDateFormatted + '0000';
+    
+    // Construct the URL for the API call
+    const url = `https://web-api.tp.entsoe.eu/api?documentType=A44&in_Domain=10YBE----------2&out_Domain=10YBE----------2&periodStart=${formattedStart}&periodEnd=${formattedEnd}&securityToken=${API_TOKEN}`;
+
+    // Make the API call
+    axios.get(url)
+    .then(response => {
+        const xml = response.data;
+       
+        const parser = new xml2js.Parser();
+        parser.parseString(xml, (err, result) => {
+          if (err) {
+            console.error(err);
+            return;
+          }
+
+          const timeSeries = result.Publication_MarketDocument.TimeSeries;
+
+          // Extract hourly prices from the API response
+          const hourlyPrices = timeSeries.flatMap(series => {
+            const date2 = String(series.Period[0].timeInterval[0].start);
+            if (typeof date2 === 'string' && date2.includes('T')) {
+  
+    const dateParts = date2.split('T')[0].split('-');
+
+
+    const date = `${dateParts[2]}-${dateParts[1]}-${dateParts[0]}`;
+    
+    
+//     const prices = series.Period[0].Point.map(point => parseFloat(point['price.amount'][0]));
+
+const prices = series.Period[0].Point.map(point => {
+  const price = parseFloat(point['price.amount'][0]) / 1000;
+  return price.toLocaleString('en-US', { minimumFractionDigits: 5, maximumFractionDigits: 5 }).replace('.', ',');
+});
+
+console.log(prices);
+      return prices.map((price, index) => ({ date, hour: index, price }));
+
+              }
+
+          });
+         
+
+    // Add actual prices to the data array
+         data.forEach(row => {
+  let actualPrice; // Initialize actualPrice variable
+  const priceRow = hourlyPrices.find(priceRow => priceRow.date === row.date && priceRow.hour === row.hour);
+  if (priceRow) {
+    actualPrice = priceRow.price;
+  }
+  row.actualPrice = actualPrice;
+  
+});
+
+// Functie om de minuutwaarde op basis van positie in te stellen
+function setMinuutByPosition(index) {
+  switch (index % 4) {
+    case 0:
+      return 0;
+    case 1:
+      return 15;
+    case 2:
+      return 30;
+    case 3:
+      return 45;
+  }
+}
+
+// Loop door de array en voeg minuten en tijdstip toe op basis van positie
+for (let i = 0; i < data.length; i++) {
+  const hour = data[i].hour;
+  const minuut = setMinuutByPosition(i);
+  const tijdstip = hour.toString().padStart(2, '0') + ':' + minuut.toString().padStart(2, '0') + ':00';
+  data[i].minuut = minuut;
+  data[i].tijdstip = tijdstip;
+}
+
+
+const results = [];
+
+fs.createReadStream('/Users/hansbontinck/Downloads/Verbruikshistoriek_elektriciteit_541448820045269231_20230109_20230227_kwartiertotalen.csv')
+  .pipe(csv({ separator: ';' }))
+  .on('data', (row) => {
+    if (row.Register.includes('Afname')) {
+      data.forEach((item) => {
+        
+      
+        if (item.date === row['\uFEFFVan Datum'] && item.tijdstip === row['Van Tijdstip']) {
+          row.actualprice = item.actualPrice;
+          results.push(row);
+        }
+      });
+    }
+  })
+  .on('end', () => {
+  
+  
+
+// Define the path and name of the output file
+const outputPath = '/Users/hansbontinck/Downloads/output.csv';
+
+// Define the array of data to be written to the CSV file
+
+// Define the header row for the CSV file
+const headerRow = 'Van Datum;Van Tijdstip;Volume;actualprice\n';
+
+// Build the CSV file content as a string
+let csvContent = headerRow;
+for (let i = 0; i < results.length; i++) {
+  const row = results[i];
+  const newRow = `${row['\uFEFFVan Datum']};${row['Van Tijdstip']};${row.Volume};${row.actualprice}\n`;
+  csvContent += newRow;
+}
+
+// Write the CSV file to disk
+fs.writeFile(outputPath, csvContent, (err) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log('CSV file written successfully!');
+  }
+});
+});
+
+     
+        });
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  });
